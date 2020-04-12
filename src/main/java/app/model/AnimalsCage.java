f4b6aa77a5de2b813c411dbd6fef67cc19201e9b
Kolimaro <saithikus@gmail.com>
2020-04-12 09:00:09 +0800

    Initial commit

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,21 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+    private Animal dog;
+    @Autowired
+    private Timer timer;
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(dog.toString());
+        System.out.println("At:");
+        System.out.println(timer.getTime());
+        System.out.println("________________________");
+    }
+}
